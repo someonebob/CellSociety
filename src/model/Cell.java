4d003fb35fac61905c3258d060211a3d5db2aa7e
Dhruv K Patel <dkp13@duke.edu>
2017-01-30 01:01:26 -0500

    Added cell skeleton

---
+++ src/model/Cell.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Cell {
+	
+	public Cell(Rules rules, State startingState){
+		
+	}
+	
+	public void setNeighbors(Cell[][] neighbors){
+		
+	}
+	
+	public void calculateFutureState(){
+		
+	}
+	
+	public void refreshState(){
+		
+	}
+	
+	public State getCurrentState(){
+		return null;
+	}
+	
+	private State[][] getNeighborStates(){
+		return null;
+	}
+}
