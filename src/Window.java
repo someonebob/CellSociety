e4ac81fdec94af28f84cbe2f928449c6c765822d
Jesse Yue <someonebob156@gmail.com>
2017-01-30 16:19:35 -0500

    Added Window Skeleton

---
+++ src/Window.java
@@ -0,0 +1,24 @@
+import gui.Menu;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Window extends Application {
+	private Stage window;
+	private Menu menu;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		window = arg0;
+		
+		menu = new Menu();
+		
+		window.setScene(menu.getMenu());
+		window.setTitle("Cell Civilization");
+		window.show();
+	}
+
+}
