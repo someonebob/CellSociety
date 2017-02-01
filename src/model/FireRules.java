90dd061d8df82f4c8c98b3fe07ba0517d4143a67
Jesse Yue <someonebob156@gmail.com>
2017-02-01 16:40:12 -0500

    Increased Flexibility of Parser

--- src/model/FireRules.java
+++ src/model/FireRules.java
@@ -3,18 +3,27 @@ package model;
 import java.io.File;
 import java.util.Random;
 
+import org.w3c.dom.Element;
+
 import xml.XMLParser;
 
 public class FireRules extends Rules {
 	
 	private static final String FIELD = "probCatch";
+	private static final String ATTRIBUTE = "name";
 	
 	private State state;
 	private double probFire;
 	
 	public FireRules(File setupInfo) {
 		XMLParser parser = new XMLParser(setupInfo);
-		probFire = Integer.parseInt(parser.getParameters(FIELD))/100;	//change from percentage to probability
+		
+		for(int i = 0; i < parser.getParameters().getLength(); i++){
+			Element element = (Element) parser.getParameters().item(i);
+			if(parser.getAttribute(element, ATTRIBUTE).equals(FIELD)){
+				probFire = Integer.parseInt(parser.getParameters().item(0).getTextContent())/100;
+			}
+		}			
 	}
 
 	@Override
@@ -25,6 +34,9 @@ public class FireRules extends Rules {
 
 	@Override
 	public State getNewState(State[][] states) {
+		if(states.length != 3){
+			return null;
+		}
 		
 		Random chance = new Random();
 		
