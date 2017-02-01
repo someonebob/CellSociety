3a0f075eff6641e935eb285f279ea125b6bf1da7
Jesse Yue <someonebob156@gmail.com>
2017-02-01 05:27:36 -0500

    Added XML Parser

--- src/model/Rules.java
+++ src/model/Rules.java
@@ -1,14 +1,19 @@
 package model;
 
+import java.io.File;
+
 public abstract class Rules {
+	private static FireRules fireRules;
 
 	public static Rules getRules(String name) {
-		// TODO Unimplemented method
+		if(name.equals("Spreading of Fire")){
+			return fireRules;
+		}
 		return null;
 	}
 	
-	public abstract State getStartingState(String stateText);
+	public abstract State getStartingState(String stateText, int rows, int cols);
 	
-	public abstract State getNewState(Cell[][] states);
+	public abstract State getNewState(State[][] states, File setupInfo);
 
 }
