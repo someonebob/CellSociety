462948e2bc089a692d9c20e97877a343e0926cf8
Dhruv K Patel <dkp13@duke.edu>
2017-02-01 12:12:52 -0500

    Implemented Cell

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,28 +1,43 @@
 package model;
 
 public class Cell {
+	private Rules rules;
+	private State currentState, futureState;
+	private Cell[][] neighborCells;
 	
 	public Cell(Rules rules, State startingState){
-		// merge test
+		this.rules = rules;
+		this.currentState = startingState;
+		this.futureState = startingState;
+		this.neighborCells = new Cell[3][3];
 	}
 	
 	public void setNeighbors(Cell[][] neighbors){
-		
+		neighborCells = neighbors;
 	}
 	
 	public void calculateFutureState(){
-		
+		futureState = rules.getNewState(getNeighborStates());		
 	}
 	
 	public void refreshState(){
-		
+		currentState = futureState;
 	}
 	
 	public State getCurrentState(){
-		return null;
+		return currentState;
 	}
 	
 	private State[][] getNeighborStates(){
-		return null;
+		State[][] neighborStates = new State[3][3];
+		
+		for(int r = 0; r < neighborCells.length; r++){
+			for(int c = 0; c < neighborCells[0].length; c++){
+				if(neighborCells[r][c] != null && neighborCells[r][c].getCurrentState() != null)
+					neighborStates[r][c] = neighborCells[r][c].getCurrentState();
+			}
+		}
+		
+		return neighborStates;
 	}
 }
