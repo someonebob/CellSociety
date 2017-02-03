7bff2444468eb6bd8d22b856ef8f08e17a483f95
Jesse Yue <someonebob156@gmail.com>
2017-02-03 17:19:19 -0500

    Fixed Fire Rules and Made Neighborhood Return a Map

--- src/model/FireRules.java
+++ src/model/FireRules.java
@@ -14,77 +14,29 @@ import xml.XMLParser;
 public class FireRules extends Rules {
 	
 	private static final String FIELD = "probCatch";
-	private static final String ATTRIBUTE = "name";
 	
 	private double probFire;
-	private FireStates[][] fstates;
+	private XMLParser parser;
 	
 	/**
 	 * Sets up the specific parameters for the Spreading Fire simulation
 	 * @param setupInfo
 	 */
-	public FireRules(File setupInfo) {
-		XMLParser parser = new XMLParser(setupInfo);
-		
-		for(int i = 0; i < parser.getParameters().getLength(); i++){
-			Element element = (Element) parser.getParameters().item(i);
-			if(parser.getAttribute(element, ATTRIBUTE).equals(FIELD)){
-				probFire = Integer.parseInt(parser.getParameters().item(0).getTextContent())/100;
-			}
-		}			
+	public FireRules(XMLParser parser) {
+		this.parser = parser;
+		probFire = Integer.parseInt(this.parser.getParameter(FIELD))/100;		
 	}
 
 	@Override
 	public State getStartingState(String stateText) {
-		return new FireStates(Integer.parseInt(stateText));
+		return new State(parser, stateText);
 	}
 
 	@Override
-	public State getNewState(State[][] states) {
-		convertStates(states);
+	public State getNewState(Neighborhood hood) {
 		
-		Random chance = new Random();
-		//current state is a tree
-		if(fstates[1][1].getStateValue() == 1){
-			//adjacent state is burning
-			if(neighborBurning(fstates)){
-				//random number is under the value of probFire
-				if(chance.nextDouble() < probFire){
-					//next state burning
-					fstates[1][1].setStateValue(2);
-				}
-			}
-		}
-		//current state is burning
-		if(fstates[1][1].getStateValue() == 2){
-			//next state burned
-			fstates[1][1].setStateValue(0);
-			
-		}
-		//if current state is empty, stay the same
-		return fstates[1][1];
+		return null;
 	}
 	
-	/**
-	 * Converts from abstract class states to Fire States to allow specific methods of Fire States
-	 * @param states
-	 */
-	private void convertStates(State[][] states){
-		fstates = new FireStates[3][3];
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++){
-				fstates[i][j] = (FireStates)states[i][j];
-			}
-		}
-	}
-	
-	/**
-	 * Checks if any neighboring states are burning
-	 * @param fstates
-	 * @return
-	 */
-	private boolean neighborBurning(FireStates[][] fstates){
-		return fstates[0][1].getStateValue() == 2 || fstates[1][0].getStateValue() == 2 || fstates[1][2].getStateValue() == 2 || fstates[2][1].getStateValue() == 2;
-	}
 
 }
