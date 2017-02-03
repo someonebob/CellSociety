3641d9aab51d9025a1d4bff161349e610d097072
Nathaniel Brooke <neb17@duke.edu>
2017-02-03 17:07:15 -0500

    Fixed error with merge conflict of State

---
+++ src/model/State.java
@@ -0,0 +1,22 @@
+package model;
+import xml.XMLParser;
+public class State {	
+	
+	private String value;
+	private XMLParser configuration;
+	
+	public State(XMLParser configuration, String value){
+		this.configuration = configuration;
+		this.value = value;
+		
+	}
+	
+	public String getColor(){
+		return configuration.getStateColor(value);
+	}
+	
+	public String getValue(){
+		return value;
+	}
+	
+}
