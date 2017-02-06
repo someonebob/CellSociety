c4fff06c7b7379154d475991f1c46dd49b3270fb
Jesse Yue <someonebob156@gmail.com>
2017-02-05 19:59:06 -0500

    Implemented Fire Rules

--- src/simulation/FireRules.java
+++ src/simulation/FireRules.java
@@ -1,49 +1,90 @@
 package simulation;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 import java.util.Random;
-
-import org.w3c.dom.Element;
-
-import model.Cell;
 import model.Neighborhood;
 import model.Rules;
 import model.State;
 import xml.XMLParser;
+
 /**
  * Rules for Spreading Fire
+ * 
  * @author Jesse
  *
  */
 public class FireRules extends Rules {
-	
 	private static final String FIELD = "probCatch";
-	
+
+	private XMLParser configuration;
 	private double probFire;
-	private XMLParser parser;
-	
+
 	/**
 	 * Sets up the specific parameters for the Spreading Fire simulation
+	 * 
 	 * @param setupInfo
 	 */
-	public FireRules(XMLParser parser) {
-		this.parser = parser;
-		probFire = Integer.parseInt(this.parser.getParameter(FIELD))/100;		
+	public FireRules(XMLParser configuration) {
+		this.configuration = configuration;
+		probFire = Integer.parseInt(this.configuration.getParameter(FIELD)) / 100.0;
 	}
 
 	@Override
 	public State getStartingState(String stateText) {
-		return new State(parser, stateText);
+		return new FireState(configuration, stateText);
 	}
 
 	@Override
-	public State getNewState(Neighborhood hood) {
-		Map<String, Cell> neighbors = hood.getNeighbors();
-		neighbors.get(Neighborhood.NW).getCurrentState();
-		return null;
+	public FireState getNewState(Neighborhood hood) {
+		Random rand = new Random();
+
+		if (isTree(hood)) {
+			if (neighborBurning(hood)) {
+				if (rand.nextDouble() <= probFire) {
+					return new FireState(configuration, FireState.BURNING);
+				} else {
+					return new FireState(configuration, FireState.TREE);
+				}
+			} else {
+				return new FireState(configuration, FireState.TREE);
+			}
+		}
+
+		if (isBurning(hood)) {
+			return new FireState(configuration, FireState.EMPTY);
+		}
+
+		return new FireState(configuration, FireState.EMPTY);
 	}
-	
+
+	private boolean isTree(Neighborhood hood) {
+		return hood.getCenter().getCurrentState().getValue().equals(FireState.TREE);
+	}
+
+	private boolean isBurning(Neighborhood hood) {
+		return hood.getCenter().getCurrentState().getValue().equals(FireState.BURNING);
+	}
+
+	private boolean neighborBurning(Neighborhood hood) {
+		for (FireState s : getStatesAround(hood)) {
+			if (s.getValue().equals(FireState.BURNING)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private ArrayList<FireState> getStatesAround(Neighborhood hood) {
+		ArrayList<FireState> states = new ArrayList<FireState>();
+		if (hood.get(0, 1) != null)
+			states.add((FireState) hood.get(0, 1).getCurrentState());
+		if (hood.get(1, 0) != null)
+			states.add((FireState) hood.get(1, 0).getCurrentState());
+		if (hood.get(1, 2) != null)
+			states.add((FireState) hood.get(1, 2).getCurrentState());
+		if (hood.get(2, 1) != null)
+			states.add((FireState) hood.get(2, 1).getCurrentState());
+		return states;
+	}
 
 }
