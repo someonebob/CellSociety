bb0a7deb1d5656bf481618841af402c662e42d5d
Dhruv K Patel <dkp13@duke.edu>
2017-02-11 16:56:54 -0500

    fixed triangle negihborhood conflicts

--- src/model/TriangularNeighborhood.java
+++ src/model/TriangularNeighborhood.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class TriangularNeighborhood extends Neighborhood {
-
+	private boolean cellIsUpsideDown;
+	private int rowwiseOffset;
+	
+	@Override
+	public void setCenter(Cell cell, Coordinate c){
+		super.setCenter(cell, c);
+		cellIsUpsideDown = (c.getCol() + c.getRow()) % 2 == 0;
+		rowwiseOffset = cellIsUpsideDown? -1 : 1;
+	}
+	
 	/**
 	 * Returns collection of Adjacent local cell coordinates
 	 * (Edges are fully connected)
@@ -12,9 +21,9 @@ public class TriangularNeighborhood extends Neighborhood {
 	@Override
 	public Collection<Coordinate> getLocalAdjacentCoordinates() {
 		Collection<Coordinate> adjacent = new ArrayList<Coordinate>();
-		adjacent.add(new Coordinate(0, 1)); 	// Directly right
-		adjacent.add(new Coordinate(0, -1)); 	// Directly left
-		adjacent.add(new Coordinate(1, 0)); 	// Directly below
+		adjacent.add(new Coordinate(0, 1)); // right	
+		adjacent.add(new Coordinate(0, -1)); // left	
+		adjacent.add(new Coordinate(rowwiseOffset, 0)); // top or bottom (depending on flip state)
 		return adjacent;
 	}
 
@@ -24,15 +33,21 @@ public class TriangularNeighborhood extends Neighborhood {
 	 */
 	public Collection<Coordinate> getLocalDiagonalCoordinates() {
 		Collection<Coordinate> diagonal = new ArrayList<Coordinate>();
-		diagonal.add(new Coordinate(-1, 0)); 	// Directly above
-		diagonal.add(new Coordinate(-1, -1)); 	// Top left
-		diagonal.add(new Coordinate(-1, 1)); 	// Top right
-		diagonal.add(new Coordinate(0, -2));	// Middle, two left
-		diagonal.add(new Coordinate(0, 2)); 	// Middle, two right
-		diagonal.add(new Coordinate(1, -2));	// Bottom, two left
-		diagonal.add(new Coordinate(1, -1));	// Bottom, one left
-		diagonal.add(new Coordinate(1, 1));		// Bottom, one right
-		diagonal.add(new Coordinate(1, 2));		// Bottom, two right
+		// sides
+		diagonal.add(new Coordinate(0, -2)); // left
+		diagonal.add(new Coordinate(0, 2)); // right
+		
+		// 3-neighbored side
+		diagonal.add(new Coordinate(rowwiseOffset, -1)); // left
+		diagonal.add(new Coordinate(rowwiseOffset, 1)); // right
+		diagonal.add(new Coordinate(rowwiseOffset, 0)); // center
+		
+		// 5-neighbored side
+		diagonal.add(new Coordinate(-rowwiseOffset, -1)); // 1 left
+		diagonal.add(new Coordinate(-rowwiseOffset, 1)); // 1 right
+		diagonal.add(new Coordinate(-rowwiseOffset, -2)); // 2 left
+		diagonal.add(new Coordinate(-rowwiseOffset, 2)); // 2 right
+		diagonal.add(new Coordinate(-rowwiseOffset, 0)); // center
 		return diagonal;
 	}
 	
